ba07642853828a4c6b2d738c862bd7d6b9c52eae
mei-pranolo <mei.pranolo@gmail.com>
2024-07-03 20:54:25 +0700

    backbone and interface

---
+++ repository/types.go
@@ -0,0 +1,14 @@
+// This file contains types that are used in the repository layer.
+package repository
+
+type Tree struct {
+	X      int
+	Y      int
+	Height int
+}
+
+type Estate struct {
+	ID     string
+	Length int
+	Width  int
+}
